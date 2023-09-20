5fb96acaaadb4f5b6259b3134b372fae52c83de5
lauroportfolio <lauroportfolio@gmail.com>
2023-09-20 17:28:38 -0300

    first commit

---
+++ components/ExploreCard.jsx
@@ -0,0 +1,14 @@
+'use client';
+
+import { motion } from 'framer-motion'
+
+import styles from '../styles';
+import { fadeIn } from '../utils/motion';
+
+const ExploreCard = () => (
+  <div>
+    Explore Card
+  </div>
+);
+
+export default ExploreCard;
